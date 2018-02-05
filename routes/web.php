0ec6d6c9ef8cf0d21b8336244f4dc4b92b6546c4
satorsight <satorsight@gmail.com>
2018-02-05 16:15:48 +0300

    init

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//Route::get('products', function () {
+//    return response(['Product 1', 'Product 2', 'Product 3'],200);
+//});
+//
+//Route::get('products/{product}', function ($productId) {
+//    return response()->json(['productId' => "{$productId}"], 200);
+//});
+//
+//
+//Route::post('products', function() {
+//    return  response()->json([
+//        'message' => 'Create success'
+//    ], 201);
+//});
+//
+//Route::put('products/{product}', function() {
+//    return  response()->json([
+//        'message' => 'Update success'
+//    ], 200);
+//});
+//
+//Route::delete('products/{product}',function() {
+//    return  response()->json(null, 204);
+//});
