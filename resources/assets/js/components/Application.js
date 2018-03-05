750be1d9ed62d7e9ca71e5c16088ce10d58882f1
Lud0k <L!9920k>
2018-03-05 16:51:48 +0300

    debug Application.js

--- resources/assets/js/components/Application.js
+++ resources/assets/js/components/Application.js
@@ -6,18 +6,8 @@ import {BrowserRouter, Route, NavLink} from 'react-router-dom'
 import Lines from 'react-preloaders/Preloaders/Lines';
 
 import MainPage from './Pages/MainPage'
-<<<<<<< HEAD
-<<<<<<< HEAD
-import ArticlePage from './Pages/ArticlePage'
-import LoginPage from './Pages/LoginPage'
-=======
-import AuthPage from './Pages/AuthPage'
-import ArticlePage from './Pages/ArticlePage'
->>>>>>> d322e0707874bfc429dd85045c4213ef36654981
-=======
 import ArticlePage from './Pages/ArticlePage'
 import AuthPage from './Pages/AuthPage'
->>>>>>> 190ff27ee45d27c04a378b81a873e69e6f098e08
 import MagazinePage from './Pages/MagazinePage'
 
 export default class Application extends Component {
@@ -31,18 +21,8 @@ export default class Application extends Component {
                 <BrowserRouter>
                     <div>
                         <Route exact path="/" component={MainPage}/>
-<<<<<<< HEAD
-<<<<<<< HEAD
-                        <Route path="/article" component={ArticlePage}/>
-                        <Route path="/login" component={LoginPage}/>
-=======
-                        <Route path="/auth" component={AuthPage}/>
-                        <Route path="/article" component={ArticlePage}/>
->>>>>>> d322e0707874bfc429dd85045c4213ef36654981
-=======
                         <Route path="/article" component={ArticlePage}/>
                         <Route path="/auth" component={AuthPage}/>
->>>>>>> 190ff27ee45d27c04a378b81a873e69e6f098e08
                         <Route path="/magazine" component={MagazinePage}/>
                     </div>
                 </BrowserRouter>
