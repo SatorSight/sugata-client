724eb583ddf44ce367968bdacb860c2ccc0fee4f
Lud0k <L!9920k>
2018-03-16 12:47:26 +0300

    disable zoom PRD-1014

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -5,7 +5,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="HandheldFriendly" content="true" />
+    <meta name="MobileOptimized" content="320" />
     <title>Laravel</title>
     <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
     <meta name="csrf-token" content="{{ csrf_token() }}">
