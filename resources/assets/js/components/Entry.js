e0b13fc81a3e6c6b1df2ff42ed1454b4acc5459d
satorsight <satorsight@gmail.com>
2018-04-28 18:14:32 +0300

    fix

--- resources/assets/js/components/Entry.js
+++ resources/assets/js/components/Entry.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './Application'
+import 'whatwg-fetch';
 
 ReactDOM.render(<Application />, document.getElementById('root'));
