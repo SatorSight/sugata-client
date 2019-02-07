477403c680bcbf432fb43124e46df08291e7006a
satorsight <satorsight@gmail.com>
2019-02-07 19:04:27 +0300

    edit NewArticles

--- resources/assets/js/components/Components/NewArticles.jsx
+++ resources/assets/js/components/Components/NewArticles.jsx
@@ -100,7 +100,8 @@ class NewArticles extends Component {
                            <div className={classes.inner}>
                                 <div className={classes.over}>
                                     <Link to={`/article/${article.id}`} className={classes.title}>
-                                        {capitalize(article.title.toLowerCase())}
+                                        {article.title.toLowerCase()}
+                                        {/*{capitalize(article.title.toLowerCase())}*/}
                                     </Link>
                                 </div>
                                 <div className={classes.detailsContainer}>
